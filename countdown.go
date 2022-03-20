66d38c97bc48bb1e92fdf0cc51454df63de2864b
Nikita Kurpas <nikitakurpas@gmail.com>
2022-03-20 13:07:29 +0900

    add countdown.go w/ tests

---
+++ countdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
+
+type DefaultSleeper struct{}
+
+func (s DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
