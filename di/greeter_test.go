591c3e4ae2b12cba3d11d5bef9813a0aa99638ad
Nikita Kurpas <nikitakurpas@gmail.com>
2022-03-20 13:06:43 +0900

    add di/greeter.go w/ tests

---
+++ di/greeter_test.go
@@ -0,0 +1,18 @@
+package di
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Nik")
+
+	got := buffer.String()
+	want := "Hello, Nik"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
